5cd5d616b4c400e7ab1e95781532a3d34b4353b5
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-13 16:32:32 -0800

    add table structure
    Co-Authored by Jer Miller <gkstaff@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-
-
 $(document).ready(function () {
   $("form#some-form").submit(function (event) {
     const someInput = $("input#some-input").val();
-    alert(someInput)
+    $("#letterBody").hide();
+    $("#letterBody").fadeIn(2000);
+    $(".displayInput").text(someInput);
+
     event.preventDefault();
   });
 });
+
+
 
 
 
